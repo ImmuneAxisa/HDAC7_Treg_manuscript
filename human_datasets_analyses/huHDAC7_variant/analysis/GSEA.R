9b97cf6d63837227090539b5ae53d8e5121bb4e9
ppaxisa <36903724+ppaxisa@users.noreply.github.com>
2022-12-05 14:43:56 +0100

    document gseaplot2 usage

--- human_datasets_analyses/huHDAC7_variant/analysis/GSEA.R
+++ human_datasets_analyses/huHDAC7_variant/analysis/GSEA.R
@@ -98,7 +98,7 @@ plotGseaTable(pathways.hallmark[topPathways], RNKvariantVector, fgseaVarH,
 ########################
 
 # Same analysis with the DOSE wrapper that provides a nice interface for making enrichment plots
-# the gseaplot2 has been removed from recent versions so I saved the source code to use it.
+# the gseaplot2 from enrichplot package not available on R3.5 so I sourced the function.
 
 detach("package:dplyr", unload = TRUE)
 library(clusterProfiler)
